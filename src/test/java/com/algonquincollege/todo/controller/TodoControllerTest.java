c7484140125df9ccff121c7080459c593ac95e50
Eric Mignardi <mignardi.e@gmail.com>
2024-11-11 11:13:06 -0500

    Minor changes

--- src/test/java/com/algonquincollege/todo/controller/TodoControllerTest.java
+++ src/test/java/com/algonquincollege/todo/controller/TodoControllerTest.java
@@ -1,7 +1,32 @@
 package com.algonquincollege.todo.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@AutoConfigureMockMvc
 class TodoControllerTest {
 
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.3");
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void connectionEstablished() {
+        assertThat(mySQLContainer.isCreated()).isTrue();
+        assertThat(mySQLContainer.isRunning()).isTrue();
+    }
 }
