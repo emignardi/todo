402acd02c5ea83c065e3f2bf34b549b82b666412
Eric Mignardi <mignardi.e@gmail.com>
2024-11-11 11:06:52 -0500

    Setup TodoServiceTest

--- src/test/java/com/algonquincollege/todo/service/TodoServiceTest.java
+++ src/test/java/com/algonquincollege/todo/service/TodoServiceTest.java
@@ -1,7 +1,24 @@
 package com.algonquincollege.todo.service;
 
+import com.algonquincollege.todo.model.Todo;
+import com.algonquincollege.todo.repository.TodoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodoServiceTest {
+
+    @InjectMocks
+    private TodoService todoService;
+    @Mock
+    private TodoRepository todoRepository;
+    @Captor
+    private ArgumentCaptor<Todo> todoCaptor;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
 }
